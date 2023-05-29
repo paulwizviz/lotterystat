4e346182196689a9293ed23e5565a75591e3271c
Paul Sitoh <paulwizviz@gmail.com>
2023-05-29 15:19:56 +0100

    Update euro package
    Add logs to DownloadFromURL function and processCSV function.
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- internal/euro/csv.go
+++ internal/euro/csv.go
@@ -10,12 +10,13 @@ import (
 	"net/http"
 
 	"github.com/paulwizviz/lotterystat/internal/csvutil"
-
 	"golang.org/x/exp/slog"
 )
 
+// DrawFromURL implements function to download draw results from source url
 func DrawFromURL(ctx context.Context) (<-chan Draw, error) {
 	url := "https://www.national-lottery.co.uk/results/euromillions/draw-history/csv"
+	slog.Info("Dowloading draws csv from url", "url", url)
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, fmt.Errorf("%w: %s %s", csvutil.ErrDownloadFromURL, url, err.Error())
@@ -30,6 +31,7 @@ func DrawFromURL(ctx context.Context) (<-chan Draw, error) {
 }
 
 func processCSV(ctx context.Context, r io.Reader) <-chan Draw {
+	const logMsg = "Processing CSV"
 	c := make(chan Draw)
 	go func() {
 		cr := csv.NewReader(r)
@@ -40,62 +42,61 @@ func processCSV(ctx context.Context, r io.Reader) <-chan Draw {
 		for {
 			select {
 			case <-ctx.Done():
-				slog.Info("processCSV", "context cancelled called")
+				slog.Info(logMsg, "context", "done")
 				break loop
 			default:
 				ln++
 				rec, err := cr.Read()
 				if errors.Is(err, io.EOF) {
-					slog.Info("processCSV", io.EOF)
 					break loop
 				}
 				if err != nil {
-					slog.Error("processCSV", err.Error())
+					slog.Error(logMsg, "error", err.Error())
 					continue loop
 				}
 				drawDate, err := csvutil.ParseDateTime(rec[0])
 				if err != nil {
-					slog.Error("processCSV", err.Error())
+					slog.Error(logMsg, "error", err.Error())
 					continue loop
 				}
 				b1, err := csvutil.ParseDrawNum(rec[1])
 				if err != nil {
-					slog.Error("processCSV", err.Error())
+					slog.Error(logMsg, "error", err.Error())
 					continue loop
 				}
 				b2, err := csvutil.ParseDrawNum(rec[2])
 				if err != nil {
-					slog.Error("processCSV", err.Error())
+					slog.Error(logMsg, "error", err.Error())
 					continue loop
 				}
 				b3, err := csvutil.ParseDrawNum(rec[3])
 				if err != nil {
-					slog.Error("processCSV", err.Error())
+					slog.Error(logMsg, "error", err.Error())
 					continue loop
 				}
 				b4, err := csvutil.ParseDrawNum(rec[4])
 				if err != nil {
-					slog.Error("processCSV", err.Error())
+					slog.Error(logMsg, "error", err.Error())
 					continue loop
 				}
 				b5, err := csvutil.ParseDrawNum(rec[5])
 				if err != nil {
-					slog.Error("processCSV", err.Error())
+					slog.Error(logMsg, "error", err.Error())
 					continue loop
 				}
 				ls1, err := csvutil.ParseDrawNum(rec[6])
 				if err != nil {
-					slog.Error("processCSV", err.Error())
+					slog.Error(logMsg, "error", err.Error())
 					continue loop
 				}
 				ls2, err := csvutil.ParseDrawNum(rec[7])
 				if err != nil {
-					slog.Error("processCSV", err.Error())
+					slog.Error(logMsg, "error", err.Error())
 					continue loop
 				}
 				dn, err := csvutil.ParseDrawSeq(rec[10])
 				if err != nil {
-					slog.Error("processCSV", err.Error())
+					slog.Error(logMsg, "error", err.Error())
 					continue loop
 				}
 				c <- Draw{
