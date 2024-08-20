c9e2fe50e073c7e9ac5269943d9ab7dd3451b938
Paul Sitoh <paulwizviz@gmail.com>
2024-08-20 14:41:45 +0100

    Update euro package
    Fix csv unit test.
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- internal/euro/csv_test.go
+++ internal/euro/csv_test.go
@@ -1,25 +1,30 @@
 package euro
 
-// func Example_processCSV() {
-// 	input := []byte(`DrawDate,Ball 1,Ball 2,Ball 3,Ball 4,Ball 5,Lucky Star 1,Lucky Star 2,UK Millionaire Maker,DrawNumber
-// 29-Sep-2023,9,11,13,21,32,2,7,"HQSB24670",1672
-// 26-Sep-2023,2,6,14,19,23,5,7,"VPRC26636",1671
-// 22-Sep-2023,3,23,24,34,35,5,8,"HNRB16622",1670
-// 19-Sep-2023,10,15,31,41,42,2,5,"JMQP30657",1669
-// 15-Sep-2023,12,14,21,45,48,8,11,"HLQH38434,HLQJ62979,HLQK11974,TKPP96754,VKPN30889,VLQB24044,XKPN60194,XLPX34097,ZLPX51278,ZLPZ96812",1668`)
+import (
+	"bytes"
+	"context"
+	"fmt"
+)
 
-// 	r := bytes.NewReader(input)
-// 	recs := csvutil.ExtractRec(context.TODO(), r)
+func Example_processCSV() {
+	input := []byte(`DrawDate,Ball 1,Ball 2,Ball 3,Ball 4,Ball 5,Lucky Star 1,Lucky Star 2,UK Millionaire Maker,DrawNumber
+29-Sep-2023,9,11,13,21,32,2,7,"HQSB24670",1672
+26-Sep-2023,2,6,14,19,23,5,7,"VPRC26636",1671
+22-Sep-2023,3,23,24,34,35,5,8,"HNRB16622",1670
+19-Sep-2023,10,15,31,41,42,2,5,"JMQP30657",1669
+15-Sep-2023,12,14,21,45,48,8,11,"HLQH38434,HLQJ62979,HLQK11974,TKPP96754,VKPN30889,VLQB24044,XKPN60194,XLPX34097,ZLPX51278,ZLPZ96812",1668`)
 
-// 	outputs := processCSV(recs)
-// 	for output := range outputs {
-// 		fmt.Println(output)
-// 	}
+	r := bytes.NewReader(input)
 
-// 	// Output:
-// 	// {{2023-09-29 00:00:00 +0000 UTC Friday 9 11 13 21 32 2 7 HQSB24670 1672} <nil>}
-// 	// {{2023-09-26 00:00:00 +0000 UTC Tuesday 2 6 14 19 23 5 7 VPRC26636 1671} <nil>}
-// 	// {{2023-09-22 00:00:00 +0000 UTC Friday 3 23 24 34 35 5 8 HNRB16622 1670} <nil>}
-// 	// {{2023-09-19 00:00:00 +0000 UTC Tuesday 10 15 31 41 42 2 5 JMQP30657 1669} <nil>}
-// 	// {{2023-09-15 00:00:00 +0000 UTC Friday 12 14 21 45 48 8 11 HLQH38434,HLQJ62979,HLQK11974,TKPP96754,VKPN30889,VLQB24044,XKPN60194,XLPX34097,ZLPX51278,ZLPZ96812 1668} <nil>}
-// }
+	outputs := processCSV(context.TODO(), r)
+	for output := range outputs {
+		fmt.Println(output)
+	}
+
+	// Output:
+	// {{2023-09-29 00:00:00 +0000 UTC Friday 9 11 13 21 32 2 7 HQSB24670 1672} <nil>}
+	// {{2023-09-26 00:00:00 +0000 UTC Tuesday 2 6 14 19 23 5 7 VPRC26636 1671} <nil>}
+	// {{2023-09-22 00:00:00 +0000 UTC Friday 3 23 24 34 35 5 8 HNRB16622 1670} <nil>}
+	// {{2023-09-19 00:00:00 +0000 UTC Tuesday 10 15 31 41 42 2 5 JMQP30657 1669} <nil>}
+	// {{2023-09-15 00:00:00 +0000 UTC Friday 12 14 21 45 48 8 11 HLQH38434,HLQJ62979,HLQK11974,TKPP96754,VKPN30889,VLQB24044,XKPN60194,XLPX34097,ZLPX51278,ZLPZ96812 1668} <nil>}
+}
