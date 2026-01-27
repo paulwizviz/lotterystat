68ed6a7016af3c06c7ed2bca784f9d997b8ab3ea
Paul Sitoh <paulwizviz@gmail.com>
2026-01-27 23:02:00 +0000

    Modify config package
    - Reduce it to simplify it to only locate ebz sqlite data file
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,18 +4,10 @@ package config
 import (
 	"errors"
 	"fmt"
-	"log"
 	"os"
 	"path"
-	"runtime"
 
-	"github.com/spf13/viper"
-	"gopkg.in/yaml.v2"
-)
-
-const (
-	SQLiteFile      = "sqlite.db"
-	SettingFileName = "settings.yaml"
+	"github.com/paulwizviz/lotterystat/internal/sqlops"
 )
 
 var (
@@ -25,48 +17,13 @@ var (
 var locationFunc = location
 
 const (
-	configType = "yaml"
-	configName = "setting"
-	dbName     = "data.db"
+	dbName = "lottery.db"
 )
-
-func Path() string {
-	return viper.GetString("path")
-}
-
-func Name() string {
-	return viper.GetString("name")
-}
-
-func DBConn() string {
-	return viper.GetString("db_conn")
-}
-
-type Detail struct {
-	Path   string `yaml:"path"`
-	Name   string `yaml:"name"`
-	DBConn string `yaml:"db_conn"`
-}
 
 func Initilalize() error {
 	p, err := locationFunc()
 	if err != nil {
-		return fmt.Errorf("%w-%s", ErrConfig, err.Error())
-	}
-	// DConn please refer to https://github.com/mattn/go-sqlite3 for list of valid strings
-	d := Detail{
-		Path:   p,
-		Name:   configName,
-		DBConn: fmt.Sprintf("file:%s/%s", p, dbName),
-	}
-	cFile := path.Join(p, fmt.Sprintf("%s.%s", d.Name, configType))
-	_, err = os.Stat(cFile)
-	if errors.Is(err, os.ErrExist) {
-		err := initViper(p)
-		if err != nil {
-			return err
-		}
-		return nil
+		return fmt.Errorf("%w:%w", ErrConfig, err)
 	}
 	_, err = os.Stat(p)
 	if errors.Is(err, os.ErrNotExist) {
@@ -75,58 +32,26 @@ func Initilalize() error {
 			return fmt.Errorf("%w-%s", ErrConfig, err.Error())
 		}
 	}
-	b, err := yaml.Marshal(d)
+
+	dbFile := path.Join(p, dbName)
+	f, err := os.Create(dbFile)
 	if err != nil {
 		return fmt.Errorf("%w-%s", ErrConfig, err.Error())
 	}
-	f, err := os.Create(cFile)
-	if err != nil {
-		return fmt.Errorf("%w-%s", ErrConfig, err.Error())
-	}
-	defer f.Close()
-	_, err = f.Write(b)
-	if err != nil {
-		return fmt.Errorf("%w-%s", ErrConfig, err.Error())
-	}
-	err = initViper(p)
+	f.Close()
+
+	_, err = sqlops.NewSQLiteFile(dbFile)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func initViper(p string) error {
-	viper.AddConfigPath(p)
-	viper.SetConfigType(configType)
-	viper.SetConfigName(configName)
-	err := viper.ReadInConfig()
-	if err != nil {
-		return fmt.Errorf("%w-%s", ErrConfig, err.Error())
-	}
-	log.Printf("%s used", viper.ConfigFileUsed())
-	return nil
-}
-
-// location returns $HOME/.bz or %APPDATA%/ebz
+// location returns $HOME/.ebz
 func location() (string, error) {
-	return locationForOS(runtime.GOOS)
-}
-
-func locationForOS(goos string) (string, error) {
-	var dir string
-	var err error
-	switch goos {
-	case "windows":
-		dir = os.Getenv("APPDATA")
-		if dir == "" {
-			return "", fmt.Errorf("%w-APPDATA not set", ErrConfig)
-		}
-	default:
-		dir, err = os.UserHomeDir()
-		if err != nil {
-			return "", fmt.Errorf("%w-unable to get home dir", ErrConfig)
-		}
-		dir = path.Join(dir, ".config")
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("%w-unable to get home dir", ErrConfig)
 	}
-	return path.Join(dir, "ebz"), nil
+	return path.Join(dir, ".ebz"), nil
 }
--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -9,62 +9,16 @@ import (
 )
 
 func TestLocation(t *testing.T) {
-	testCases := []struct {
-		name     string
-		goos     string
-		setup    func(t *testing.T)
-		expected func(t *testing.T) string
-	}{
-		{
-			name: "Windows",
-			goos: "windows",
-			setup: func(t *testing.T) {
-				t.Setenv("APPDATA", "C:\\Users\\test\\AppData\\Roaming")
-			},
-			expected: func(t *testing.T) string {
-				return path.Join("C:\\Users\\test\\AppData\\Roaming", "ebz")
-			},
-		},
-		{
-			name: "Linux",
-			goos: "linux",
-			setup: func(t *testing.T) {
-				// No setup needed for linux
-			},
-			expected: func(t *testing.T) string {
-				home, err := os.UserHomeDir()
-				if err != nil {
-					t.Fatalf("Failed to get user home dir: %v", err)
-				}
-				return path.Join(home, ".config", "ebz")
-			},
-		},
-		{
-			name: "Darwin",
-			goos: "darwin",
-			setup: func(t *testing.T) {
-				// No setup needed for darwin
-			},
-			expected: func(t *testing.T) string {
-				home, err := os.UserHomeDir()
-				if err != nil {
-					t.Fatalf("Failed to get user home dir: %v", err)
-				}
-				return path.Join(home, ".config", "ebz")
-			},
-		},
+	home, err := os.UserHomeDir()
+	if err != nil {
+		t.Fatalf("Failed to get user home dir: %v", err)
 	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			tc.setup(t)
-			actual, err := locationForOS(tc.goos)
-			if err != nil {
-				t.Fatalf("locationForOS failed: %v", err)
-			}
-			assert.Equal(t, tc.expected(t), actual)
-		})
+	expected := path.Join(home, ".ebz")
+	actual, err := location()
+	if err != nil {
+		t.Fatalf("location failed: %v", err)
 	}
+	assert.Equal(t, expected, actual)
 }
 func TestInitialize(t *testing.T) {
 	tmpDir, err := os.MkdirTemp("", "test-config")
@@ -76,7 +30,7 @@ func TestInitialize(t *testing.T) {
 	// Set environment variable to control config location
 	oldLocationFunc := locationFunc
 	locationFunc = func() (string, error) {
-		return path.Join(tmpDir, "ebz"), nil
+		return path.Join(tmpDir, ".ebz"), nil
 	}
 	t.Cleanup(func() {
 		locationFunc = oldLocationFunc
@@ -88,16 +42,12 @@ func TestInitialize(t *testing.T) {
 	}
 
 	// Verify config file and directory
-	configDir := path.Join(tmpDir, "ebz")
+	configDir := path.Join(tmpDir, ".ebz")
 
-	configFile := path.Join(configDir, "setting.yaml")
-	_, err = os.Stat(configFile)
+	dbFile := path.Join(configDir, "lottery.db")
+	_, err = os.Stat(dbFile)
 	if err != nil {
-		t.Fatalf("Config file not created: %v", err)
+		t.Fatalf("db file not created: %v", err)
 	}
 
-	// Verify config content
-	assert.Equal(t, configDir, Path(), "Path should be correct")
-	assert.Equal(t, "setting", Name(), "Name should be correct")
-	assert.Equal(t, "file:"+path.Join(configDir, "data.db"), DBConn(), "DBConn should be correct")
 }
